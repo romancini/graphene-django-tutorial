4d438091e3285aa59e6d6197e4cc8ced2824c90f
Romancini <romancini@hpe.com>
2017-06-01 12:01:04 -0300

    Relay

--- ingredients/schema.py
+++ ingredients/schema.py
@@ -1,46 +1,31 @@
 import graphene
+from graphene import relay, ObjectType, AbstractType
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from ingredients.models import Category, Ingredient
 
 
-class CategoryType(DjangoObjectType):
+class CategoryNode(DjangoObjectType):
     class Meta:
         model = Category
+        filter_fields = ['name', 'ingredients']
+        interfaces = (relay.Node, )
 
 
-class IngredientType(DjangoObjectType):
+class IngredientNode(DjangoObjectType):
     class Meta:
         model = Ingredient
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['exact', 'icontains'],
+            'category': ['exact'],
+            'category__name': ['exact']
+        }
+        interfaces = (relay.Node, )
 
 
 class Query(graphene.AbstractType):
-    category = graphene.Field(CategoryType, id=graphene.Int(), name=graphene.String())
-    all_categories = graphene.List(CategoryType)
-    ingredient = graphene.Field(IngredientType, id=graphene.Int(), name=graphene.String())
-    all_ingredients = graphene.List(IngredientType)
-
-    def resolve_category(self, args, context, info):
-        id = args.get('id')
-        name = args.get('name')
-
-        if id is not None:
-            return Category.objects.get(pk=id)
-
-        if name is not None:
-            return Category.objects.get(name=name)
-
-    def resolve_all_categories(self, args, context, info):
-        return Category.objects.all()
-
-    def resolve_ingredient(self, args, context, info):
-        id = args.get('id')
-        name = args.get('name')
-
-        if id is not None:
-            return Ingredient.objects.get(pk=id)
-
-        if name is not None:
-            return Ingredient.objects.get(name=name)
-
-    def resolve_all_ingredients(self, args, context, info):
-        return Ingredient.objects.select_related('category').all()
+    category = relay.Node.Field(CategoryNode)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
+    ingredient = relay.Node.Field(IngredientNode)
+    all_ingredients = DjangoFilterConnectionField(IngredientNode)
